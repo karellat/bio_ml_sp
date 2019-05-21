f603bff46d294d628e480b1e51e64e02ad038518
Jakub Mifek <jakub.mifek@lutherx.com>
2019-05-21 17:47:22 +0200

    batch

--- model.py
+++ model.py
@@ -100,7 +100,7 @@ class Network:
             callbacks=[self.tb_callback]
         )
     def predict(self, data_images, args):
-        return  self.model.predict(x = data_images)
+        return  self.model.predict(x = data_images.batch(args.batch_size))
 
 if __name__ == "__main__":
     import argparse
